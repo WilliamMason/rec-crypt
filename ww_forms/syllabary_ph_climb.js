0192b1ca3d26b62fd7b255b32e2a462e988418a4
WilliamMason <mason.wk@gmail.com>
2021-10-22 11:25:23 -0700

    Update syllabary_ph_climb.js
    fixed custom tet table bug

--- ww_forms/syllabary_ph_climb.js
+++ ww_forms/syllabary_ph_climb.js
@@ -129,7 +129,7 @@ function make_table(str) {
         c = str.charAt(i);
         n = alpha.indexOf(c);
         if ( n == -1) continue; //not a letter
-        if (state = 0) {
+        if (state == 0) {
             n1 = n;
             c1 = c;
         }
