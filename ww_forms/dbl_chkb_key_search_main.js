03871c6b47b51a74799bb00c93026b93273602e9
WilliamMason <mason.wk@gmail.com>
2022-12-12 13:28:57 -0800

    Add files via upload
    changed word_list_array routine so you don't have to reload word list if you change to the option require all key letters to be in ciphertext.

--- ww_forms/dbl_chkb_key_search_main.js
+++ ww_forms/dbl_chkb_key_search_main.js
@@ -161,8 +161,16 @@ function do_search(){
    // note: below, you need the .buffer at the end because word_list_array is a (char) view of the arrayBuffer, not
    // the arrayBuffer itself. If word_list_array was just an arrayBuffer you wouldn't need to add .buffer to it.
    //worker.webkitPostMessage( {op_choice:1, buf:word_list_array.buffer},[word_list_array.buffer]);
-   worker.postMessage( {op_choice:1, buf:word_list_array.buffer},[word_list_array.buffer]);
+   //worker.postMessage( {op_choice:1, buf:word_list_array.buffer},[word_list_array.buffer]);
    //worker.webkitPostMessage( {op_choice:2, str:str, str2:str2});
+   
+// send copy of word_list_array, not word_list_array itself, so word_list_array won't be deleted
+   var buf = new ArrayBuffer(word_list_array.length);
+   var bufView = new Uint8Array(buf);
+   for (i=0;i< word_list_array.length;i++)
+        bufView[i] = word_list_array[i];
+   worker.postMessage( {op_choice:1, buf:buf},[buf]);
+
    worker.postMessage( {op_choice:2, str:str, str2:str2, kw:kw, flag:flag});
 }
 
