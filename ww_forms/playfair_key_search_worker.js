38a2ac8b3be84e5fc4e47fbe53880a41e593d4f3
WilliamMason <mason.wk@gmail.com>
2023-04-28 12:23:08 -0700

    Add files via upload
    added a reverse key option to playfair hill-climber key search

--- ww_forms/playfair_key_search_worker.js
+++ ww_forms/playfair_key_search_worker.js
@@ -24,6 +24,7 @@ var buf_len;
 var noise_step, cycle_limit, begin_level;
 var fudge_factor = 0.2; // for backup in case I forget to send it.
 //var period=5; // default
+var reversed_key_flag = false;
 
 var crib_flag=0;
 var crib;
@@ -264,6 +265,9 @@ function get_key_array(wrd){
             if (  used_indx[i] == 0 )
                 key[indx++] = i
         }
+		if (reversed_key_flag)
+			key.reverse();
+		
 }
 
  
@@ -325,7 +329,10 @@ function do_key_search(str){
                 out_str += '\nKey: ';
                 for (i=0;i<25;i++) 
                     out_str += alpha.charAt(work_key[i]);
-                out_str += "\nroute: "+k+" ("+route_name[k]+")";
+				if ( reversed_key_flag)
+					out_str += "\nroute: "+k+" ("+route_name[k]+" reversed)";
+				else 
+					out_str += "\nroute: "+k+" ("+route_name[k]+")";				
                 //document.getElementById('output_area').value = out_str;	
                 postMessage(out_str);
             }
@@ -351,6 +358,7 @@ onmessage = function(event) { //receiving a message
   else if (state == 2){
     //word_pattern_string = event.data.str;
     str = event.data.str;
+	reversed_key_flag = event.data.reversed_key_flag;		
     do_key_search(str);
   }
 }
